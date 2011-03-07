b7b73d4e74a9d1dfc5e2c8017997730b71c9133d
Sajal Kayan <sajal@sajal-desktop.(none)>
2011-03-07 17:53:04 +0700

    make the full loader anonymous. to not pollute global namespace

--- dfp-lab-regular.js
+++ dfp-lab-regular.js
@@ -13,74 +13,77 @@ INCOMPLETE SCRIPT : WILL BREAK YOUR SITE
 
 //debug
 
-var targets = [];
-function docwrt(str){
-        console.log(str);
-  var script = str.replace(/(.*)\=\"/g, '').replace(/\"(.*)/g, '');
-        //console.log(script.match(/cookie\.js/g));
-  if (script.match(/cookie\.js/g)){
-          //console.log("cookiejs")
-    $LAB.script(script).wait();
-  } else {
-    $LAB.script(script).wait(function(){
-      
-      // following function makes the magic happen!
-      
-      // lets load the ads
-      console.log(adslots);
-      if (adslots){
-        for (var x in adslots){
-          console.log(adslots[x]["pubid"] + " - " + adslots[x]["slotname"]);
-          GA_googleAddSlot(adslots[x]["pubid"], adslots[x]["slotname"]);
-        }
-      }
-      GA_googleFetchAds();
-      // now comes the fun part.. rendering them
-      var Wrapper_GA_googleFillSlot = function(targetad){
-        var slotname  = targetad["slotname"];
-        var targetloc  = targetad["target"];
-        targets.push = {"slotname": slotname, "targetloc": targetloc};
-        var docwrttemp = function(str){
-          var adscript = str.match(/[\"|\'](.*)[\"|\']/g, '');
-          adscript = adscript[0].replace(/\"/g, '');
-          adscript = adscript.replace(/\'/g, "");
-          console.log(targetloc + " - " + str);
-          console.log(targetloc + " - " + adscript);
-          
-          function renderad(str){
-            //try to guess slotname to determine target
-            for (y in adslots){
-              console.log(adslots[y].slotname);
-              if (str.match(adslots[y].slotname)){
-                console.log("yay");
-                var targetdiv = adslots[y].target;
-              }
-            }
-            console.log(targetdiv + " - " + str)
-            target = document.getElementById(targetdiv);
-            target.innerHTML = str            
+(function(){
+  var targets = [];
+  function docwrt(str){
+          console.log(str);
+    var script = str.replace(/(.*)\=\"/g, '').replace(/\"(.*)/g, '');
+          //console.log(script.match(/cookie\.js/g));
+    if (script.match(/cookie\.js/g)){
+            //console.log("cookiejs")
+      $LAB.script(script).wait();
+    } else {
+      $LAB.script(script).wait(function(){
+        
+        // following function makes the magic happen!
+        
+        // lets load the ads
+        console.log(adslots);
+        if (adslots){
+          for (var x in adslots){
+            console.log(adslots[x]["pubid"] + " - " + adslots[x]["slotname"]);
+            GA_googleAddSlot(adslots[x]["pubid"], adslots[x]["slotname"]);
           }
-          document.write = renderad;    
-          
-          $LAB.script(adscript).wait();
+        }
+        GA_googleFetchAds();
+        // now comes the fun part.. rendering them
+        var Wrapper_GA_googleFillSlot = function(targetad){
+          var slotname  = targetad["slotname"];
+          var targetloc  = targetad["target"];
+          targets.push = {"slotname": slotname, "targetloc": targetloc};
+          var docwrttemp = function(str){
+            var adscript = str.match(/[\"|\'](.*)[\"|\']/g, '');
+            adscript = adscript[0].replace(/\"/g, '');
+            adscript = adscript.replace(/\'/g, "");
+            console.log(targetloc + " - " + str);
+            console.log(targetloc + " - " + adscript);
             
+            function renderad(str){
+              //try to guess slotname to determine target
+              for (y in adslots){
+                console.log(adslots[y].slotname);
+                if (str.match(adslots[y].slotname)){
+                  console.log("yay");
+                  var targetdiv = adslots[y].target;
+                }
+              }
+              console.log(targetdiv + " - " + str)
+              target = document.getElementById(targetdiv);
+              target.innerHTML = str            
+            }
+            document.write = renderad;    
+            
+            $LAB.script(adscript).wait();
+              
+          };
+          document.write = docwrttemp;
+          GA_googleFillSlot(slotname);
         };
-        document.write = docwrttemp;
-        GA_googleFillSlot(slotname);
-      };
-      for (var x in adslots){
-        console.log(adslots[x]["target"]);
-        Wrapper_GA_googleFillSlot(adslots[x]);        
-      }
-      //GA_googleAddSlot("DFPpubid", "test_async_lb");
-      //Wrapper_googleFillSlotWithSize("ca-pub-7046344781760701", "test_async_lb", 728, 90, "leaderboard");
-      //Wrapper_googleFillSlotWithSize("ca-pub-7046344781760701", "test_async_sky", 160, 600, "skyscraper");
-    });
+        for (var x in adslots){
+          console.log(adslots[x]["target"]);
+          Wrapper_GA_googleFillSlot(adslots[x]);        
+        }
+        //GA_googleAddSlot("DFPpubid", "test_async_lb");
+        //Wrapper_googleFillSlotWithSize("ca-pub-7046344781760701", "test_async_lb", 728, 90, "leaderboard");
+        //Wrapper_googleFillSlotWithSize("ca-pub-7046344781760701", "test_async_sky", 160, 600, "skyscraper");
+      });
+    }
   }
-}
+  
+  document.write = docwrt; //intercepts document.write from below script
+  $LAB.script("http://partner.googleadservices.com/gampad/google_service.js").wait(function(){
+    GS_googleAddAdSenseService(DFPpubid);
+    GS_googleEnableAllServices();
+  });
+})();
 
-document.write = docwrt; //intercepts document.write from below script
-$LAB.script("http://partner.googleadservices.com/gampad/google_service.js").wait(function(){
-  GS_googleAddAdSenseService(DFPpubid);
-  GS_googleEnableAllServices();
-});
